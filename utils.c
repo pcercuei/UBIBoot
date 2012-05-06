06b527833bce4d12f775438f9404c171e64812d9
Paul Cercueil <paul@crapouillou.net>
2012-05-06 04:56:53 +0200

    Added implementation of memcpy.

--- utils.c
+++ utils.c
@@ -10,3 +10,15 @@ int strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return 0;
 }
+
+void * memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char *d = dest;
+	const unsigned char *s = src;
+
+	while (n) {
+		*d++ = *s++;
+		n -= sizeof(unsigned char);
+	}
+	return dest;
+}
