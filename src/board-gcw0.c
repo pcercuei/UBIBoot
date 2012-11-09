86332dbf15820a6b2a7a3198e958b465eeaa7ad2
Paul Cercueil <paul@crapouillou.net>
2012-11-09 14:03:42 -0300

    GCW0: Update main clock dividers for better performance
    The DDR2 clock will now be 166 MHz instead of 125 MHz.

--- src/board-gcw0.c
+++ src/board-gcw0.c
@@ -14,10 +14,10 @@
 
 /* Authorized values: 1 2 3 4 6 8 12 */
 #define CFG_CDIV  1
-#define CFG_H0DIV 8
-#define CFG_H1DIV 8
-#define CFG_H2DIV 8
-#define CFG_PDIV  8
+#define CFG_H0DIV 6
+#define CFG_H1DIV 3
+#define CFG_H2DIV 6
+#define CFG_PDIV  6
 #define CFG_C1DIV 2
 
 
