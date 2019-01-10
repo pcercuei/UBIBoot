3210a0cd58a862e3ce895b8cda7220f2caaea16c
Paul Cercueil <paul@crapouillou.net>
2019-01-10 17:18:05 -0300

    Add header bytes to allow NAND boot on jz4725b
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- src/config-rs90.h
+++ src/config-rs90.h
@@ -16,3 +16,8 @@
 /* MMC parameters */
 #define MMC_ID 1
 #define MMC_1BIT 1
+
+/* NAND parameters */
+#define PAGE_SIZE 2048 /* 512, 2048 or 4096 */
+#define BUS_WIDTH 8 /* 8 or 16 */
+#define ROW_CYCLE 3 /* 3 or 2 */
