675392e56452544e2919d84d9063d47f53dad46e
Paul Cercueil <paul@crapouillou.net>
2021-02-28 22:09:47 +0000

    DDR2_H5PS1G63EFR-S6C.h: Update timings
    We only use DDR2-400, so we can configure for CL=3. Furthermore, the
    timings listed in the header file were way conservative. Update them to
    the recommended values listed in the datasheet.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- src/DDR2_H5PS1G63EFR-S6C.h
+++ src/DDR2_H5PS1G63EFR-S6C.h
@@ -16,24 +16,24 @@
 #define DDR_ROW   13 /* ROW : 12 to 14 row address */
 #define DDR_COL   10 /* COL :  8 to 10 column address */
 #define DDR_BANK8  1 /* Banks each chip: 0-4bank, 1-8bank */
-#define DDR_CL     5 /* CAS latency: 1 to 5 */
+#define DDR_CL     3 /* CAS latency: 1 to 6 */
 
 /*
  * DDR2 controller timing1 register, unit: ps
  */
-#define DDR_tRAS  60000  /* ACTIVE to PRECHARGE command period to the same bank. */
-#define DDR_tRTP  20000  /* READ to PRECHARGE command period. */
-#define DDR_tRP   20000  /* PRECHARGE command period to the same bank */
-#define DDR_tRCD  20000  /* ACTIVE to READ or WRITE command period to the same bank. */
-#define DDR_tRC   80000  /* ACTIVE to ACTIVE command period to the same bank.*/
-#define DDR_tRRD  18000  /* ACTIVE bank A to ACTIVE bank B command period. */
-#define DDR_tWR   25000  /* WRITE Recovery Time defined by register MR of DDR2 memory */
-#define DDR_tWTR  12000  /* WRITE to READ command delay. */
+#define DDR_tRAS  40000  /* ACTIVE to PRECHARGE command period to the same bank. */
+#define DDR_tRTP   7500  /* READ to PRECHARGE command period. */
+#define DDR_tRP   15000  /* PRECHARGE command period to the same bank */
+#define DDR_tRCD  15000  /* ACTIVE to READ or WRITE command period to the same bank. */
+#define DDR_tRC   55000  /* ACTIVE to ACTIVE command period to the same bank.*/
+#define DDR_tRRD   7500  /* ACTIVE bank A to ACTIVE bank B command period. */
+#define DDR_tWR   15000  /* WRITE Recovery Time defined by register MR of DDR2 memory */
+#define DDR_tWTR   7500  /* WRITE to READ command delay. */
 
 /*
  * DDR2 controller timing2 register
  */
-#define DDR_tRFC   128000 /* ps,  AUTO-REFRESH command period. */
+#define DDR_tRFC    75000 /* ps,  AUTO-REFRESH command period. */
 #define DDR_tMINSR    140 /* tCK, Minimum Self-Refresh / Deep-Power-Down */
 #define DDR_tXP         2 /* tCK, EXIT-POWER-DOWN to next valid command period: 1 to 8 tCK. */
 #define DDR_tMRD        4 /* tCK, Load-Mode-Register to next valid command period: 1 to 4 tCK */
